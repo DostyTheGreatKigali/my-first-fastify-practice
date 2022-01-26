838365151508efc3b432129a16f7fcd1000ccf28
DostyTheGreatKigali <abumucal89@gmail.com>
2022-01-26 11:50:15 +0000

    Attempting to readcsv

---
+++ controller/readCSV.js
@@ -0,0 +1,15 @@
+// var fs = require('fs'); 
+// var parse = require('csv-parse');
+// var parser = parse({columns: true}, function (err, records) {
+// 	console.log(records);
+// });
+
+// fs.createReadStream(process.cwd() +'/downloads/downloads.csv').pipe(parser);
+
+// module.exports = {
+//     getAllBlogs,
+//     getBlog,
+//     addBlog,
+//     updateBlog,
+//     deleteBlog
+// }
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const fastify = require('fastify')({
     logger: true
 })
 
+// var fs = require('fs'); 
+// var parse = require('csv-parser');
+
 // Use Fastify Env plugin: https://github.com/fastify/fastify-env
 // const fastifyEnv = require('fastify-env') // load plugin
 
@@ -34,21 +37,50 @@ const fastify = require('fastify')({
 fastify.get('/', function (req, reply) {
     // DOWNLOAD A FILE
     // https://stackoverflow.com/questions/11944932/how-to-download-a-file-with-node-js-without-using-third-party-libraries
+    // const http = require('https'); // or 'https' for https:// URLs
+    // const fs = require('fs');
     // const file = fs.createWriteStream("oct.csv");
     // const request = http.get("https://s3-eu-west-1.amazonaws.com/tf-trans/2029/inflow_OCT-2021.csv", function(response) {
     // response.pipe(file);
     // });
-    reply.send({ hello: 'world' })
-})
 
-// Register routes to handle blog posts
-const blogRoutes = require('./routes/blogs')
-blogRoutes.forEach((route, index) => {
-    fastify.route(route)
-})
+    // READ CSV FILE
+    // var fs = require('fs'); 
+    // var parse = require('csv-parser');
+    // var parser = parse({columns: true}, function (err, records) {
+    //     console.log(records);
+    // });
 
-const http = require('https'); // or 'https' for https:// URLs
-const fs = require('fs');
+    // fs.createReadStream(__dirname + '/downloads/downloads.csv').pipe(parser);
+
+    // (A) CSV PARSER MODULE
+    // npm install csv-parser
+    // https://www.npmjs.com/package/csv-parser
+    // const fs = require("fs"),
+    // csv = require("csv-parser");
+
+    // // (B) READ CSV FILE
+    // var results = [];
+    // fs.createReadStream(__dirname + '/downloads/downloads.csv')
+    // .pipe(csv())
+    // .on("data", (data) => results.push(data))
+    // .on("end", () => {
+    // console.log(results);
+    // });
+
+        reply.send({ hello: 'world' })
+    })
+
+
+
+    // Register routes to handle blog posts
+    const blogRoutes = require('./routes/blogs')
+    blogRoutes.forEach((route, index) => {
+        fastify.route(route)
+    })
+
+// const http = require('https'); // or 'https' for https:// URLs
+// const fs = require('fs');
 
 // const file = fs.createWriteStream("oct.csv");
 // const request = http.get("https://s3-eu-west-1.amazonaws.com/tf-trans/2029/inflow_OCT-2021.csv", function(response) {
