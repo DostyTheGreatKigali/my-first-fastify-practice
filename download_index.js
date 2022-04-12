b7e906c57c6f86e45a740052367ef0d88739f3b6
DostyTheGreatKigali <abumucal89@gmail.com>
2022-04-12 23:00:20 +0000

    Comment added for current month in words

--- download_index.js
+++ download_index.js
@@ -30,6 +30,8 @@ const downloadFile = async (url, fileFullPath) =>{
 }
 
 let yearToSelect = new Date().getFullYear().toString();
+// CURRENT MONTH IN WWORDS
+// https://flaviocopes.com/how-to-get-month-from-javascript-date/
 let monthToSelect = (new Date().toLocaleString('default', { month: 'short' })).toUpperCase();
 console.log("Month selected " + monthToSelect);
 // let monthToSelect = (new Date().getMonth() + 1).toString().padStart(2, '0');
